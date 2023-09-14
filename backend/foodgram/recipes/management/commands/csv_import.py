22d9b04ba6a4e844e8a8ef037c8acdc8ad8d7e47
Miron Sadykov <reagent992@gmail.com>
2023-09-14 22:14:10 +0300

    remake csv import management command

--- backend/foodgram/recipes/management/commands/csv_import.py
+++ backend/foodgram/recipes/management/commands/csv_import.py
@@ -1,80 +1,69 @@
-# # flake8: noqa
-# import csv
-#
-# from django.core.management import BaseCommand
-#
-# from recipes.models import Ingredients
-#
-# TODO: переписать
-# class Command(BaseCommand):
-#     help = 'Загрузка ингредиентов и единиц измерения из CSV-файла'
-#
-#     def add_arguments(self, parser):
-#         parser.add_argument(
-#             'csv_file_path',
-#             nargs='?',
-#             type=str,
-#             default='recipes/management/commands/ingredients.csv')
-#
-#     def handle(self, *args, **options):
-#         csv_file_path = options['csv_file_path']
-#         with open(csv_file_path, encoding='UTF-8') as csvfile:
-#             """
-#             Скрипт добавления ингредиентов и
-#             единиц измерения в БД из CSV-файла.
-#             """
-#             reader = csv.reader(csvfile)
-#             counter_ingredients = 0
-#             counter_measurment_units = 0
-#             ingredient_already_in_db = 0
-#             errors_list = []
-#             for row in reader:
-#                 if len(row) != 2:
-#                     raise ValueError(
-#                         f'Неверные данные в таблице, строка: {row}')
-#
-#                 csv_file_ingredient = row[0]
-#                 csv_file_measurment_unit = row[1]
-#
-#                 # Добавление единицы измерения
-#                 try:
-#                     measurment_unit_obj, created_measurment_unit_status = (
-#                         MeasurementUnit.objects.get_or_create(
-#                             name=csv_file_measurment_unit
-#                         ))
-#                     if created_measurment_unit_status:
-#                         counter_measurment_units += 1
-#                 except Exception as e:
-#                     errors_list.append(f'Ошибка {e} на строке {row}')
-#
-#                 # Добавление ингредиента
-#                 try:
-#                     ingredient_obj, created_ingredient_status = (
-#                         Ingredients.objects.get_or_create(
-#                             name=csv_file_ingredient,
-#                             measurement_unit=measurment_unit_obj
-#                         ))
-#                     if created_ingredient_status:
-#                         counter_ingredients += 1
-#                     elif not created_ingredient_status:
-#                         ingredient_already_in_db += 1
-#                 except Exception as e:
-#                     errors_list.append(f'Ошибка {e} на строке {row}')
-#
-#             self.stdout.write(
-#                 '-------------------------Итог:----------------------------')
-#             self.stdout.write(self.style.SUCCESS(
-#                 f'Введено {counter_ingredients} ингредиентов'
-#             ))
-#             self.stdout.write(self.style.SUCCESS(
-#                 f'Введено {counter_measurment_units} единиц измерения'
-#             ))
-#             self.stdout.write(self.style.WARNING(
-#                 f'{ingredient_already_in_db} '
-#                 f'ингредиентов уже было в БД.'
-#             ))
-#             print()
-#             if errors_list:
-#                 self.stdout.write(self.style.ERROR('Ошибки:'))
-#                 for e in errors_list:
-#                     print(e)
+import csv
+import os
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from recipes.models import Ingredients, Tag
+
+
+def read(file_path):
+    """Получение информации из файла."""
+
+    try:
+        with open(file_path, encoding='UTF-8') as csvfile:
+            tags = list()
+            for row in csv.reader(csvfile):
+                tags.append(row)
+    except FileNotFoundError:
+        print(f"Файл не найден по пути {file_path}")
+
+    return tags
+
+
+def write_ingredients(ingredients_array):
+    """Запись ингредиентов."""
+
+    counter = 0
+    for name, measurement_unit in ingredients_array:
+        obj, status = Ingredients.objects.get_or_create(
+            name=name,
+            measurement_unit=measurement_unit
+        )
+        if status:
+            counter += 1
+
+    return counter
+
+
+def write_tags(tags_array):
+    """Запись тегов."""
+
+    counter = 0
+    for name, color, slug in tags_array:
+        obj, status = Tag.objects.get_or_create(
+            name=name,
+            color=color,
+            slug=slug
+        )
+        if status:
+            counter += 1
+
+    return counter
+
+
+class Command(BaseCommand):
+    help = 'Загрузка ингредиентов и единиц измерения из CSV-файла'
+
+    def handle(self, *args, **options):
+        ingredients_csv_file_path = os.path.join(
+            settings.BASE_DIR.parent, 'data/ingredients.csv')
+        tags_csv_file_path = os.path.join(
+            settings.BASE_DIR.parent, 'data/tags.csv')
+        ingredients_array = read(ingredients_csv_file_path)
+        tags_array = read(tags_csv_file_path)
+        ingredients_write_result = write_ingredients(ingredients_array)
+        tags_write_result = write_tags(tags_array)
+        print('---------------------------Итог-------------------------------')
+        print(f'Введено {ingredients_write_result} ингредиентов')
+        print(f'Введено {tags_write_result} тегов')
